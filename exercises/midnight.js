a974ec0a8dedbe835f9000b980a75dac5c869028
sgp1986 <sgp1229@gmail.com>
2023-01-27 20:00:04 -0700

    new exercises

---
+++ exercises/midnight.js
@@ -0,0 +1,99 @@
+/*
+the timeOfDay function takes a time argument in minutes and returns the time in 24 hour format
+positive numbers is after midnight, negative is before
+reimplement the below function using the Date object
+*/
+
+// const MINUTES_PER_HOUR = 60;
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+// function timeOfDay(deltaMinutes) {
+//   deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
+//   if (deltaMinutes < 0) {
+//     deltaMinutes = MINUTES_PER_DAY + deltaMinutes;
+//   }
+
+//   let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
+//   let minutes = deltaMinutes % MINUTES_PER_HOUR;
+
+//   return `${padWithZeroes(hours, 2)}:${padWithZeroes(minutes, 2)}`;
+// }
+
+// function padWithZeroes(number, length) {
+//   let numberString = String(number);
+
+//   while (numberString.length < length) {
+//     numberString = `0${numberString}`;
+//   }
+
+//   return numberString;
+// }
+
+// const MILLISECONDS_PER_MINUTE = 60000;
+
+// function timeOfDay(deltaMinutes) {
+//   const midnight = new Date('1/1/2023 00:00');
+//   const afterMidnight = new Date(midnight.getTime() + deltaMinutes * MILLISECONDS_PER_MINUTE);
+//   const hours = padWithZeroes(afterMidnight.getHours(), 2);
+//   const minutes = padWithZeroes(afterMidnight.getMinutes(), 2);
+
+//   return `${hours}:${minutes}`;  
+// }
+
+// console.log(timeOfDay(0) === "00:00");
+// console.log(timeOfDay(-3) === "23:57");
+// console.log(timeOfDay(35) === "00:35");
+// console.log(timeOfDay(-1437) === "00:03");
+// console.log(timeOfDay(3000) === "02:00");
+// console.log(timeOfDay(800) === "13:20");
+// console.log(timeOfDay(-4231) === "01:29");
+
+// now refactor the code that does the opposite
+// const MINUTES_PER_HOUR = 60;
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+// function afterMidnight(timeStr) {
+//   const timeComponents = timeStr.split(':');
+//   const hours = parseInt(timeComponents[0], 10);
+//   const minutes = parseInt(timeComponents[1], 10);
+
+//   return hours * MINUTES_PER_HOUR + minutes;
+// }
+
+// function beforeMidnight(timeStr) {
+//   let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+//   if (deltaMinutes === MINUTES_PER_DAY) {
+//     deltaMinutes = 0;
+//   }
+
+//   return deltaMinutes;
+// }
+
+const MILLISECONDS_PER_MINUTE = 60000;
+const MINUTES_PER_HOUR = 60;
+const HOURS_PER_DAY = 24;
+const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+const DATE_PART = '1/1/2000';
+
+function afterMidnight(timeStr) {
+  const midnight = new Date(`${DATE_PART} 00:00`);
+  const currentDateTime = new Date(`${DATE_PART} ${timeStr}`);
+
+  return (currentDateTime.getTime() - midnight.getTime()) / MILLISECONDS_PER_MINUTE;
+}
+
+function beforeMidnight(timeStr) {
+  let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+  if (deltaMinutes === MINUTES_PER_DAY) {
+    deltaMinutes = 0;
+  }
+
+  return deltaMinutes;
+}
+
+console.log(afterMidnight('00:00'));       // 0
+console.log(beforeMidnight('00:00'));      // 0
+console.log(afterMidnight('12:34'));       // 754
+console.log(beforeMidnight('12:34'));      // 686
